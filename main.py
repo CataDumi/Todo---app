c7cfc7f75765c4eea8b1133ae62a3aadeadca44e
CataDumi <cata.dumi.134@gmail.com>
2023-01-16 20:24:15 +0200

    added title and subheader

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def update_activity(content):
 with open ('todos.txt','r') as file:
     content=file.readlines()
 
+st.title('Todo App')
+st.subheader('This is my todo app ')
+st.write('The app is made to study "streamlib". ')
+
 for index, item in enumerate(content):
     # print(item)
     checkbox=st.checkbox(item.strip('\n'),key=item)
@@ -26,5 +30,8 @@ for index, item in enumerate(content):
         del st.session_state[item]
         st.experimental_rerun()
 
-st.session_state
-st.text_input(label='Aici bagi text',placeholder='Continua sa scrii',on_change=add_activity,key='new_item')
+# st.session_state
+st.text_input(label='Enter a new activity',
+              placeholder='Write an activity',
+              on_change=add_activity,
+              key='new_item')
